630e6568bc6b194990bcba36d225772c4e2e2157
AdityaThadeshwar <aditya.thadeshwar@gmail.com>
2023-04-15 17:32:12 +0530

    Added Delete and Put mapping for customer

--- src/main/java/com/aditya/insurance/management/system/service/CustomerService.java
+++ src/main/java/com/aditya/insurance/management/system/service/CustomerService.java
@@ -22,16 +22,17 @@ public class CustomerService {
         this.customerRepository = customerRepository;
     }
 
-    //Return all customers
+    //Retrieve all customers
     @GetMapping("/v1/customers")
     public List<Customer> findAllCustomers() {
         return customerRepository.findAll();
     }
 
-    //Return customer by ID
+    //Retrieve customer by ID
     @GetMapping("/v1/customers/{id}")
     public Customer findCustomerByID(@PathVariable int id) {
 
+        //Check if customer exists
         Optional<Customer> customer = customerRepository.findById(id);
 
         if(customer.isEmpty()) {
@@ -42,10 +43,35 @@ public class CustomerService {
     }
 
     //Add new Customer
-    @PostMapping("/v1/customer")
-    public Customer createCustomer(@Valid @RequestBody Customer customer) {
+    @PostMapping("/v1/customers")
+    public ResponseEntity<Customer> createCustomer(@Valid @RequestBody Customer customer) {
         Customer savedCustomer = customerRepository.save(customer);
 
-        return savedCustomer;
+        return ResponseEntity.ok(savedCustomer);
+    }
+
+    //Delete customer by ID
+    @DeleteMapping("/v1/customers/{id}")
+    public void deleteCustomerById(@PathVariable int id) {
+        customerRepository.deleteById(id);
+    }
+
+    //Update customer by ID
+    @PutMapping("/v1/customers/{id}")
+    public ResponseEntity<Customer> updateCustomerByID(@PathVariable int id, @RequestBody Customer customer) {
+
+        //Check if customer exists
+        Optional<Customer> updateCustomer = customerRepository.findById(id);
+
+        if(updateCustomer.isEmpty()) {
+            throw new CustomerNotFoundException("Customer with id " + id + " not found");
+        }
+        updateCustomer.get().setName(customer.getName());
+        updateCustomer.get().setDateOfBirth(customer.getDateOfBirth());
+        updateCustomer.get().setMobileNo(customer.getMobileNo());
+
+        customerRepository.save(updateCustomer.get());
+
+        return ResponseEntity.ok(updateCustomer.get());
     }
 }
