78aa9927d08d04e7de40479dd2e7b99d472ff7d4
AdityaThadeshwar <aditya.thadeshwar@gmail.com>
2023-04-15 18:36:43 +0530

    Added Get mapping to search policy by id and Post mapping to add a policy for a customer

---
+++ src/main/java/com/aditya/insurance/management/system/exceptions/PolicyNotFoundException.java
@@ -0,0 +1,6 @@
+package com.aditya.insurance.management.system.exceptions;
+
+public class PolicyNotFoundException extends RuntimeException {
+    public PolicyNotFoundException(String s) {
+    }
+}
--- src/main/java/com/aditya/insurance/management/system/service/PolicyService.java
+++ src/main/java/com/aditya/insurance/management/system/service/PolicyService.java
@@ -2,26 +2,64 @@ package com.aditya.insurance.management.system.service;
 
 import com.aditya.insurance.management.system.entity.Customer;
 import com.aditya.insurance.management.system.entity.Policy;
+import com.aditya.insurance.management.system.exceptions.CustomerNotFoundException;
+import com.aditya.insurance.management.system.exceptions.PolicyNotFoundException;
+import com.aditya.insurance.management.system.repository.CustomerRepository;
 import com.aditya.insurance.management.system.repository.PolicyRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class PolicyService {
 
     PolicyRepository policyRepository;
+    CustomerRepository customerRepository;
 
     @Autowired
-    public PolicyService(PolicyRepository policyRepository) {
+    public PolicyService(PolicyRepository policyRepository, CustomerRepository customerRepository) {
         this.policyRepository = policyRepository;
+        this.customerRepository = customerRepository;
     }
 
     //Retrieve all policies
     @GetMapping("/v1/policies")
     public List<Policy> findAllCustomers() {
         return policyRepository.findAll();
+    }
+
+    //Retrieve policy by ID
+    @GetMapping("/v1/policies/{id}")
+    public Policy findCustomerByID(@PathVariable int id) {
+
+        //Check if customer exists
+        Optional<Policy> policy = policyRepository.findById(id);
+
+        if(policy.isEmpty()) {
+            throw new PolicyNotFoundException("Customer with id " + id + " not found");
+        }
+
+        return policy.get();
+    }
+
+    //Add new Policy
+    @PostMapping("/v1/customers/{id}/policies")
+    public ResponseEntity<Policy> createCustomer(@PathVariable int id, @Valid @RequestBody Policy policy) {
+
+        //Check if customer exists
+        Optional<Customer> savedCustomer = customerRepository.findById(id);
+
+        if(savedCustomer.isEmpty()) {
+            throw new CustomerNotFoundException("Customer with id " + id + " not found");
+        }
+
+        policy.setCustomer(savedCustomer.get());
+        Policy savedPolicy = policyRepository.save(policy);
+
+        return ResponseEntity.ok(savedPolicy);
     }
 }
